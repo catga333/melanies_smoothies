c3b43e3392079dd5a5c2316321e5505e6825aaf6
catga333 <catga@outlook.com>
2024-12-05 10:09:09 +0000

    Update streamlit_app.py
    sf_df code update from sf.df

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     
 
